74331324c8638adc506c83f59814e46d04f386c7
HiYasin <hello.mr.faisal@gmail.com>
2024-09-24 18:44:12 +0600

    Tab switching functionality Home-Blog

--- scripts/tabSwitch.js
+++ scripts/tabSwitch.js
@@ -1,32 +1,46 @@
 const donationBtn = document.getElementById('donationBtn');
 const historyBtn = document.getElementById('historyBtn');
-
 const donationTab = document.getElementById('donation');
 const historyTab = document.getElementById('history');
 
 
-function showTab(btnId, tabId) {
-    // Hide all tabs
-    donationTab.classList.add('hidden');
-    historyTab.classList.add('hidden');
-
-    // Reset button styles
-    donationBtn.classList.remove('bg-green');
-    historyBtn.classList.remove('bg-green');
-
-    // Add button active states styles
-    btnId.classList.add('bg-green');
-
-    // Show required tab
-    tabId.classList.remove('hidden');
-}
-
 donationBtn.addEventListener('click', function(event){
     event.preventDefault();
-    showTab(donationBtn,donationTab);
+    historyTab.classList.add('hidden');
+    donationTab.classList.remove('hidden');
+    historyBtn.classList.remove('bg-green');
+    donationBtn.classList.add('bg-green');
 });
 
 historyBtn.addEventListener('click', function(event){
     event.preventDefault();
-    showTab(historyBtn,historyTab);
+    donationTab.classList.add('hidden');
+    historyTab.classList.remove('hidden');
+    donationBtn.classList.remove('bg-green');
+    historyBtn.classList.add('bg-green');
 });
+
+const blog = document.getElementById('blog');
+const blogTab = document.getElementById('faq');
+const home = document.getElementById('home');
+const btns = document.getElementById('buttons')
+
+blog.addEventListener('click',function(event){
+    event.preventDefault();
+    donationTab.classList.add('hidden');
+    btns.classList.add('hidden');
+    blog.classList.add('hidden');
+    home.classList.remove('hidden');
+    blogTab.classList.remove('hidden');
+})
+
+home.addEventListener('click',function(event){
+    event.preventDefault();
+    donationTab.classList.remove('hidden');
+    donationBtn.classList.add('bg-green');
+    historyBtn.classList.remove('bg-green');
+    btns.classList.remove('hidden');
+    blog.classList.remove('hidden');
+    home.classList.add('hidden');
+    blogTab.classList.add('hidden');
+})
